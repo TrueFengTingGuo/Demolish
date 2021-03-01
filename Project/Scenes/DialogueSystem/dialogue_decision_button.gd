1e4354e694859288fdd208c95eef84cf7390b379
FengTing Guo <15019668+TrueFengTingGuo@users.noreply.github.com>
2021-02-28 21:49:01 -0500

    update dialogue system

--- Project/Scenes/DialogueSystem/dialogue_decision_button.gd
+++ Project/Scenes/DialogueSystem/dialogue_decision_button.gd
@@ -1,19 +1,11 @@
 extends Button
 
-
+signal on_next_conversation(nextConversation_index)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 var next_conversation = 0
 
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Button_pressed():
 
-	emit_signal("on_next_conversation", next_conversation)
+	emit_signal("on_next_conversation", int(next_conversation))
